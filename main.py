ee8913e19db6cd363cf228fc9e84b8a346d36262
dylanlu0604-dot <dylanlu0604@gmail.com>
2026-02-28 17:26:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def main():
         if module_name == "utils": continue
         
         # 🌟 重新加回：選取部分爬蟲測試
-        target_scrapers = ["kgi"] 
+        target_scrapers = ["wallstreetcn"] 
 
         if module_name not in target_scrapers: 
             continue 
