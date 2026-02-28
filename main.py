dfae52986a44554b9568869bf188f0233e90355e
dylanlu0604-dot <dylanlu0604@gmail.com>
2026-02-28 16:59:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def main():
         if module_name == "utils": continue
         
         # 🌟 重新加回：選取部分爬蟲測試
-        target_scrapers = ["mega"] 
+        target_scrapers = ["ubot"] 
 
         if module_name not in target_scrapers: 
             continue 
